01c52e9c064b10daa2323c31f8f96c1ab545684c
Puja <pujayadav@gmail.com>
2020-09-28 21:23:23 +0530

    asking to start the game

---
+++ UC1_Game Started/game started/game started/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace game_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string snakeladder = null;
+            int start = 0;
+            int max = 100;
+            int current_score = 0;
+            int currentposition = start;
+            Console.WriteLine(" welcome to the snakeladder game:");
+            Console.WriteLine("Do you want to play snake ladder");
+            Console.ReadLine();
+           
+               
+        }
+    }
+}
