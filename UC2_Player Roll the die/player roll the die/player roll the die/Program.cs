c81dca779f33efa85986c2f173bbe34df9e5c226
Puja <pujayadav@gmail.com>
2020-09-28 21:36:35 +0530

    player throw a dice

---
+++ UC2_Player Roll the die/player roll the die/player roll the die/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace player_roll_the_die
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string snakeladder = null;
+            int start = 0;
+            int max = 100;
+            int current_score = 0;
+            int currentposition = start;
+            Console.WriteLine(" welcome to the snakeladder game:");
+            Console.WriteLine("Do you want to play snake ladder");
+          snakeladder=  Console.ReadLine();
+            
+            if(snakeladder=="yes")
+            {
+                Random random = new Random();
+                int randomnumber = random.Next(1, 7);
+                Console.WriteLine("throwing a dice.....number is:"+randomnumber);
+            }
+            else
+            {
+                Console.WriteLine("player choose not to play");
+            }
+        }
+    }
+}
